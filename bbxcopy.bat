3b6e6c821a6892d8d6ca12f366648b1d025d5e0e
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-04-15 14:45:23 -0500

    Add files via upload

---
+++ bbxcopy.bat
@@ -0,0 +1,8 @@
+:: Set Source Below 
+set source=C:\Users\Atlink\Desktop\BBBScript\Source\BB_Copy_Files\*
+
+:: Set Destination Below 
+set dest=C:\Users\Atlink\Desktop\BBBScript\Dest\BB_Copy_Files\
+
+:: Execution Script Below 
+xcopy /D /S %source% %dest%
