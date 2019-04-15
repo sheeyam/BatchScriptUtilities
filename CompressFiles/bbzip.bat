5d6763108ea2ed6b740d3c85e453b86af25d597c
sheeyam <sheeyam.shellvacumar@gmail.com>
2019-04-15 14:48:09 -0500

    no message

--- CompressFiles/bbzip.bat
+++ CompressFiles/bbzip.bat
@@ -1,11 +1,11 @@
 :: Set Source Below 
-set source=C:\Users\Atlink\Desktop\BBBScript\Source\BB_Files
+set source=C:\Users\Sheeyam\Desktop\BBBScript\Source\BB_Files
 
 :: Set File Name Below
 set name=BB_Files_%DATE:~-10,2%%DATE:~-7,2%%DATE:~-4%
 
 :: Set Destination Below 
-set dest=C:\Users\Atlink\Desktop\BBBScript\Dest\%name%.zip
+set dest=C:\Users\Sheeyam\Desktop\BBBScript\Dest\%name%.zip
 
 :: Execution Script Below 
 CScript zip.vbs %source% %dest%
---
+++ CopyFiles/bbxcopy.bat
@@ -0,0 +1,8 @@
+:: Set Source Below 
+set source=C:\Users\Sheeyam\Desktop\BBBScript\Source\BB_Copy_Files\*
+
+:: Set Destination Below 
+set dest=C:\Users\Sheeyam\Desktop\BBBScript\Dest\BB_Copy_Files\
+
+:: Execution Script Below 
+xcopy /D /S %source% %dest%
